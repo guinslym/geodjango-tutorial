acca7a53341bb918ed9eb11f7d74221baa579ac2
Guinsly Mondésir <guinslym@gmail.com>
2018-09-20 21:31:19 +0000

    gis

--- mygeodjango/settings.py
+++ mygeodjango/settings.py
@@ -75,24 +75,29 @@ WSGI_APPLICATION = 'mygeodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 '''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gisdata',
         'USER': 'guinslym',
-        'PASSWORD': 'mypassword',
+        'PASSWORD': 'geodjango',
         'HOST': 'localhost',
         'PORT': 5432,
     }
 }
 '''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gisdata',
+        'USER': 'guinslym',
+        'PASSWORD': 'geodjango',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
